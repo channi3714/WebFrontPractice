739351c66c3f7319fdb7361f2aabde2427cab37d
channi <dhckd3714@gmail.com>
2022-10-09 16:21:02 +0900

    22.10.09 1st commit
    responsive header update

---
+++ main.js
@@ -0,0 +1,10 @@
+main.js
+
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+const links = document.querySelector('.navbar_user');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    links.classList.toggle('active');
+})
