4ed8574b7d7f781bb7fc459924699cf75d26ba87
channi <dhckd3714@gmail.com>
2022-12-05 16:39:41 +0900

    22.12.05 4th commit
    rank page - play&stop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+window.onload = function () {
 
-const toggleBtn = document.querySelector('.navbar_toggleBtn');
-const menu = document.querySelector('.navbar_menu');
-const links = document.querySelector('.navbar_user');
+    const toggleBtn = document.querySelector('.navbar_toggleBtn');
+    const menu = document.querySelector('.navbar_menu');
+    const links = document.querySelector('.navbar_user');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    links.classList.toggle('active');
-})
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        links.classList.toggle('active');
+    })
+}
--- js/rank.js
+++ js/rank.js
@@ -1,4 +1,37 @@
+let ps = true;
+let interval;
+function playSlide(param = 0) {
+    if (param == 0) {
+        console.log(0);
+        ps = false;
+    }
+    else if (param == 1) {
+        console.log(1);
+        ps = true;
+    }
+    if (ps) {
+        interval = setInterval(function () {
+            document.getElementsByClassName("swiper-button-next")[0].click();
+        }, 2000);
+        console.log(ps);
+    }
+    else {
+        clearInterval(interval);
+        console.log("stop");
+    }
+}
+
 window.onload = function () {
+    //-----------------------main nav bar---------------------------
+    const toggleBtn = document.querySelector('.navbar_toggleBtn');
+    const menu = document.querySelector('.navbar_menu');
+    const links = document.querySelector('.navbar_user');
+
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        links.classList.toggle('active');
+    })
+    //----------------------slide-----------------------------------
     var swiper = new Swiper(".wrap", {
         pagination: {
             el: ".swiper-pagination",
